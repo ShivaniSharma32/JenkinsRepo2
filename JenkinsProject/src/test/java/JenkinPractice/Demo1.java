325efde90bd86ee4fecce49aad4cd0eaf84f87b0
ShivaniSharma23 <prachoo20@gmail.com>
2023-12-17 23:34:41 +0530

    push1

---
+++ JenkinsProject/src/test/java/JenkinPractice/Demo1.java
@@ -0,0 +1,15 @@
+package JenkinPractice;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	@Test
+	public void aTest()
+	{
+	
+		System.out.println("Hello mam"); 
+		System.out.println("network");
+		System.out.println("Ram");
+		
+}
+}
